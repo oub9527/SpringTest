aea9ddd2b5e2b55fcc065628214eb18f77c18285
superman <81718917@qq.com>
2018-07-24 09:49:44 +0800

    try git

--- src/TestClass.java
+++ src/TestClass.java
@@ -6,13 +6,6 @@
 public class TestClass {
     public static void main(String[] args) {
 
-        System.out.println("aaabbb");
-        System.out.println("cccddd");
-        System.out.println("zzz");
-        System.out.println("GitTest");
-        System.out.println("this is a second commit");
-        System.out.println("this is a branch commit");
-        System.out.println("this is a test branch");
-        System.out.println("create a branch");
+        System.out.println("try git");
     }
 }
