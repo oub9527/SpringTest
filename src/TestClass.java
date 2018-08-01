79a11cbf070444009d3b4935c04a2fa5fd22d376
ob <81718917@qq.com>
2018-08-01 11:54:31 +0800

    delete some file

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,7 +1,6 @@
 /**
  * Author: Administrator
  * Time: 2018-07-17 15:39
- * Copyright (C) 2017 Xiamen Yaxon Networks CO.,LTD.
  */
 public class TestClass {
     public static void main(String[] args) {
