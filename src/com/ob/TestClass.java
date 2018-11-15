691091da5ee062c2afac5c97ec5d216211e67193
欧斌 <171913@nd.com>
2018-11-15 18:39:09 +0800

    add file2

--- src/com/ob/TestClass.java
+++ src/com/ob/TestClass.java
@@ -8,9 +8,6 @@ public class TestClass {
     public static void main(String[] args) {
 
         System.out.println("a dev branch");
-        System.out.println("a oubin branch");
-        System.out.println("merge to dev branch");
-        System.out.println("i have changed dev branch");
 
         RealTest realTest = new RealTest();
         System.out.println("stag1e12");
